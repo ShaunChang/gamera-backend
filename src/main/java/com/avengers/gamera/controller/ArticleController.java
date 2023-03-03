bbcc18069a94642cf5717ebe72ef451aa7197b8a
xiangcheng <769358935@qq.com>
2023-03-03 15:41:02 +1030

    controller test

--- src/main/java/com/avengers/gamera/controller/ArticleController.java
+++ src/main/java/com/avengers/gamera/controller/ArticleController.java
@@ -15,7 +15,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.List;
 
 @RestController
 @RequestMapping("articles")
@@ -25,16 +24,39 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @PostMapping
-    @Operation(summary = "C")
+    @Operation(summary = "Create new article")
     @ResponseStatus(HttpStatus.CREATED)
     public ArticleGetDto createArticle(@RequestBody ArticlePostDto articlePostDto) {
         return articleService.createArticle(articlePostDto);
     }
 
     @PutMapping("/update/{articleId}")
-    @Operation(summary = "U")
+    @Operation(summary = "Update cxcxcxcxc")
     @ResponseStatus(HttpStatus.OK)
     public ArticleGetDto updateArticle(@PathVariable Long articleId,@RequestBody ArticlePostDto articlePostDto){
         return articleService.updateArticle(articleId,articlePostDto);
+    }
+    @GetMapping("/pages")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<MiniArticleGetDto> getMiniArticles(@PageableDefault(size = 10) Pageable pageable){
+        return articleService.getMiniArticles(pageable);
+    }
+
+    @GetMapping("/types/{articleType}")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<MiniArticleGetDto> getMiniArticlesByType(@PathVariable ArticleType articleType, @PageableDefault(size = 10) Pageable pageable){
+        return articleService.getMiniArticlesByType(articleType,pageable);
+    }
+
+    @GetMapping("/{articleId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ArticleGetDto getArticleById(@PathVariable Long articleId) {
+        return articleService.getArticleById(articleId);
+    }
+
+    @DeleteMapping("/{articleId}")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteArticleById(@PathVariable Long articleId) {
+        return articleService.deleteArticleById(articleId);
     }
 }
